60b7a16697f24ccd781549c1fa3b50bf6c6c66cd
Jonathan Gazeley <me@jonathangazeley.com>
2024-01-10 19:54:58 +0000

    Bare minimum global config

--- camerahub/settings.py
+++ camerahub/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'iommi',
+    'taggit',
     'schema',
 ]
 
@@ -127,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+GEOPOSITION_GOOGLE_MAPS_API_KEY = ''
