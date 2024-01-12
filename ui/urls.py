d8b4c7a7ea93f553e6e198b6c5dda16237328ab6
Jonathan Gazeley <me@jonathangazeley.com>
2024-01-12 19:54:29 +0000

    Filter on all indexed columns

--- ui/urls.py
+++ ui/urls.py
@@ -17,6 +17,6 @@ register_factory(TaggableManager, shortcut_name='many_to_many')
 urlpatterns = [
     # ...your urls...
     path('', Table(auto__model=Manufacturer).as_view(), name='index'),
-    path('manufacturer/', Table(auto__model=Manufacturer, container__children__bar=html.div('Bar', after=0), columns__name__filter__include=True, actions__add=Action(attrs__href='/'), ).as_view(), name='manufacturer-list'),
+    path('manufacturer/', Table(auto__model=Manufacturer, container__children__bar=html.div('Bar', after=0), query_from_indexes=True, actions__add=Action(attrs__href='/'), ).as_view(), name='manufacturer-list'),
     path('manufacturer/create', Form.create(auto__model=Manufacturer).as_view(), name='manufacturer-create'),
 ]
