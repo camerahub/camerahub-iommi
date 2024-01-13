279563a092594b5179a9e0902eda4aab92bc286c
Jonathan Gazeley <me@jonathangazeley.com>
2024-01-13 20:44:33 +0000

    Render "add" button to tables

--- ui/tables.py
+++ ui/tables.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from iommi import Table, Action, html, Column
 from schema.models import Manufacturer
 
@@ -6,4 +7,4 @@ def manufacturer_table(request):
     class ManufacturerTable(Table):
         select = Column.select()  # Shortcut for creating checkboxes to select rows
 
-    return ManufacturerTable(auto__model=Manufacturer, container__children__bar=html.div('Bar', after=0), query_from_indexes=True, actions__add=Action(attrs__href='/'))
+    return ManufacturerTable(auto__model=Manufacturer, container__children__bar=html.div('Bar', after=0), query_from_indexes=True, actions__add=Action(attrs__href=reverse_lazy('manufacturer-create')))
