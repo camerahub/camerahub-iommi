af713c74a613d63265edcbc2adf24ccce8fcc292
Jonathan Gazeley <me@jonathangazeley.com>
2024-01-18 22:13:39 +0000

    Basic detail view

---
+++ ui/pages.py
@@ -0,0 +1,15 @@
+from django.shortcuts import get_object_or_404
+from django.template import Template, loader
+from iommi import Page, html
+from schema.models import Manufacturer
+
+def manufacturer_view(request, slug):
+    obj = get_object_or_404(Manufacturer, slug=slug)
+    context={'object':obj}
+    template = loader.get_template("detail/manufacturer_detail.html")
+
+    class ManufacturerPage(Page):
+        title = html.h1(obj.name)
+        body = Template(template.render(context, request))
+    
+    return ManufacturerPage(context=context)
--- ui/urls.py
+++ ui/urls.py
@@ -8,6 +8,8 @@ from taggit.managers import TaggableManager
 
 # Import any models you need from your models.  Here I'm using Album
 from schema.models import Manufacturer
+
+from .pages import manufacturer_view
 from .tables import manufacturer_table
 
 # Workaround for https://github.com/iommirocks/iommi/issues/339
@@ -15,9 +17,15 @@ register_factory(GenericRelation, factory=None)
 register_factory(GenericForeignKey, factory=None)
 register_factory(TaggableManager, shortcut_name='many_to_many')
 
+register_path_decoding(
+    manufacturer_name=Manufacturer.name,
+    manufacturer_pk=Manufacturer,
+)
+
 urlpatterns = [
     # ...your urls...
     path('', Table(auto__model=Manufacturer).as_view(), name='index'),
     path('manufacturer/', manufacturer_table, name='manufacturer-list'),
     path('manufacturer/create', Form.create(auto__model=Manufacturer).as_view(), name='manufacturer-create'),
+    path('manufacturer/<slug>/', manufacturer_view)
 ]
